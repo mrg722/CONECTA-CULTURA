fffbbd290b99adc52d5e3ddf7781ffb66e4915f6
mrg722 <mar.reyesg@duocuc.cl>
2026-09-25 11:17:30 -0300

    Delete conecta-culturav2 directory

--- conecta-culturav2/assets/js/app.js
+++
@@ -1,7 +0,0 @@
-// Selección del botón interactivo
-const botonBienvenida = document.querySelector("#boton-bienvenida");
-
-// Escucha del evento click con mensaje personalizado mejorado (Desafío Individual)
-botonBienvenida.addEventListener("click", () => {
-  alert("¡Bienvenido/a a Conecta Cultura! Durante este semestre construiremos esta plataforma paso a paso para conectar a la comunidad con el arte, el conocimiento y el bienestar.");
-});
